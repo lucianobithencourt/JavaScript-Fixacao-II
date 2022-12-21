b3c916d0844ae6e6431586198a02c92d04276671
Luciano Bitencourt <lucbite@gmail.com>
2022-12-21 20:16:06 -0300

    update
    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
 
-var  nome = window.prompt("Informe seu Nome","Digite aqui");
-
-var idade = Number(prompt("Informe sua idade","Digite aqui"))
-
-var peso = parseFloat(prompt("Informe seu peso","Digite aqui"))
-
-confirm("Confirma as informações digitadas ? ")
-
-// console
-console.log(`O srº(ª) ${nome} tem ${idade} anos e pesa ${peso}Kg`);
-
-
-// screen html
-document.writeln(`O srº(ª) ${nome} tem ${idade} anos e pesa ${peso}Kg`);
+// Exercicio de fixacao - Calculo de IMC
 
