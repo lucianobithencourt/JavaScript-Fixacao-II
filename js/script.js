9cc685709380242c71b3f38a5790e81e3eec0113
Luciano Bitencourt <lucbite@gmail.com>
2022-12-21 19:16:00 -0300

    update
    update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+var  nome = window.prompt("Informe seu Nome","Digite aqui");
+
+var idade = Number(prompt("Informe sua idade","Digite aqui"))
+
+var peso = parseFloat(prompt("Informe seu peso","Digite aqui"))
+
+confirm("Confirma as informações digitadas ? ")
+
+// console
+console.log(`O srº(ª) ${nome} tem ${idade} anos e pesa ${peso}Kg`);
+
+
+// screen html
+document.writeln(`O srº(ª) ${nome} tem ${idade} anos e pesa ${peso}Kg`);
+
